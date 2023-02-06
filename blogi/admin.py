326413d2595aa0815811ea5cd32e02a8403dcaa6
VictoriaLind <victoria@lindstar.fi>
2023-02-06 11:43:44 +0200

    update

---
+++ blogi/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Postaus
+
+# Register your models here.
+
+#This makes it so that the admin page shows the Postaus model
+#specifically the @admin.register(Postaus) part
+@admin.register(Postaus)
+class PostausAdmin(admin.ModelAdmin):
+    #Makes it so that the admin page shows the title and the time it was created
+    list_display = ["otsikko", "luotu"]
+    
---
+++ blogi/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Postaus(models.Model):
+    otsikko = models.CharField(max_length=200)
+    teksti = models.TextField()
+    luotu = models.DateTimeField(auto_now_add=True)
+
+
+    # This method is used to display the title of the post in the admin panel
+    def __str__(self):
+        return self.otsikko
