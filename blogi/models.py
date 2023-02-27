3b6adba245de0ebe2084d8956890300237c14741
VictoriaLind <victoria@lindstar.fi>
2023-02-27 17:00:28 +0200

    27.2.2023

--- blogi/models.py
+++ blogi/models.py
@@ -5,6 +5,7 @@ class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
     teksti = models.TextField()
     luotu = models.DateTimeField(auto_now_add=True)
+    kuva = models.ImageField(null=True)
 
     # This method is used to display the title of the post in the admin panel
     def __str__(self):
