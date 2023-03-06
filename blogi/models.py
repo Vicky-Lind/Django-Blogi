e5994147ac4e9054ad6d095b0b512f9f43b86aa4
VictoriaLind <victoria@lindstar.fi>
2023-03-06 15:59:19 +0200

    final 6.3.2023

--- blogi/models.py
+++ blogi/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
+    ingressi = models.TextField(blank=True)
     teksti = models.TextField()
-    kuva = models.ImageField(null=True)
+    kuva = models.ImageField(upload_to="blogi/kuvat", null=True, blank=True)
     luotu = models.DateTimeField(auto_now_add=True)
 
-    # This method is used to display the title of the post in the admin panel
     def __str__(self):
         return self.otsikko
 
+    def luo_ingressi(self):
+        if self.ingressi:
+            return self.ingressi
+        return self.teksti[:50] + "..."
--- koodausblogi/urls.py
+++ koodausblogi/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from blogi import views as blogi_views
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('', blogi_views.postaukset, name="postauslista"),
     path('postaus/<int:id>', blogi_views.nayta_postaus, name="nayta_postaus"),
     path('uusi/', blogi_views.uusi_postaus, name="uusi_postaus"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
